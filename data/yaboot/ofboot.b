9c325794600ab5a8d3889d38ac79a504fb0fc08d
Unknown Author <debian-cd@lists.debian.org>
2001-07-10 14:42:54 +0000

    This commit was manufactured by cvs2svn to create tag 'debian-cd_2_2_6'.

---
+++ data/yaboot/ofboot.b
@@ -0,0 +1,13 @@
+<CHRP-BOOT>
+<COMPATIBLE>
+MacRISC
+</COMPATIBLE>
+<DESCRIPTION>
+Debian GNU/Linux 2.2 (potato) PowerPC bootloader
+</DESCRIPTION>
+<BOOT-SCRIPT>
+" screen" output
+load-base release-load-area
+boot cd:,\install\powermac\yaboot
+</BOOT-SCRIPT>
+</CHRP-BOOT>
--- tools/link.pl
+++ tools/link.pl
@@ -4,16 +4,37 @@ use strict;
 
 my $symlink_farm = $ENV{'SYMLINK'} || 0;
 my $link_verbose = $ENV{'VERBOSE'} || 0;
+my $link_copy = $ENV{'COPYLINK'} || 0;
 
 sub good_link ($$) {
 	my ($src, $dest) = @_;
+
+	# Check if the destination directory does exist
+	my $ddir = $dest;
+	$ddir =~ s#/?[^/]+$##g;
+	if ($ddir eq "") 
+	{
+		$ddir = ".";
+	}
+	if (! -d $ddir) # Create it if not
+	{
+		system("mkdir -p $ddir");
+	}
+	# Link the files
 	if ($symlink_farm) {
 		print "Symlink: $dest => $src\n" if ($link_verbose >= 3);
 		if (not symlink ($src, $dest)) {
 			print STDERR "Symlink from $src to $dest failed: $!\n";
 		}
+	} elsif ($link_copy) {
+		print "Copy: $dest => $src\n" if ($link_verbose >= 3);
+		if (system("cp -ap $src $dest")) {
+			my $err_num = $? >> 8;
+			my $sig_num = $? & 127;
+			print STDERR "Copy from $src to $dest failed: cp exited with error code $err_num, signal $sig_num\n";
+		}
 	} else {
-		print "Harlink: $dest => $src\n" if ($link_verbose >= 3);
+		print "Hardlink: $dest => $src\n" if ($link_verbose >= 3);
 		if (not link ($src, $dest)) {
 			print STDERR "Link from $src to $dest failed: $!\n";
 		}
